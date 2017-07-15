ea424d2dd4763998180c4116350d525a506b4a06
qbikkx <qbikkx@gmail.com>
2017-07-16 00:44:20 +0300

    simple task finished

--- app/src/main/java/dev/qbikkx/keepsolidone/MainActivity.java
+++ app/src/main/java/dev/qbikkx/keepsolidone/MainActivity.java
@@ -1,52 +1,104 @@
 package dev.qbikkx.keepsolidone;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_INPUT_TEXT = "input_text";
+
+    private final static int REQUEST_CODE_SEND_BTN_ACTION = 1;
+
+    private final static boolean BTN_STATE_NOT_ENABLED = false;
+    private final static boolean BTN_STATE_ENABLED = true;
+
+    private EditText inputEditText;
+    private Button sendBtn;
+    private Button clearBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        inputEditText = (EditText) findViewById(R.id.et_input);
+        sendBtn = (Button) findViewById(R.id.btn_send);
+        inputEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, R.string.secret, Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            /**
+             * Блокировка кнопок при пустом поле гарантирует, что далее будет передаваться
+             * непустая строка
+             */
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s == null || s.length() == 0) {
+                    setButtonsState(BTN_STATE_NOT_ENABLED);
+                } else if (!isButtonsEnabled()) {
+                    setButtonsState(BTN_STATE_ENABLED);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent senderIntent = new Intent(MainActivity.this, ReceiverActivity.class);
+                senderIntent.putExtra(EXTRA_INPUT_TEXT, inputEditText.getText().toString());
+                startActivityForResult(senderIntent, REQUEST_CODE_SEND_BTN_ACTION);
+            }
+        });
+        clearBtn = (Button) findViewById(R.id.btn_clear);
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputEditText.setText("");
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (REQUEST_CODE_SEND_BTN_ACTION == requestCode) {
+            switch (resultCode) {
+                case RESULT_OK:
+                    resultOk();
+                    break;
+                case RESULT_CANCELED:
+                    resultCanceled();
+                    break;
+            }
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private boolean isButtonsEnabled() {
+        return sendBtn.isEnabled();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    private void setButtonsState(boolean isEnabled) {
+        sendBtn.setEnabled(isEnabled);
+        clearBtn.setEnabled(isEnabled);
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void resultOk() {
+        inputEditText.setText("");
+        Toast.makeText(this, R.string.text_confirmed, Toast.LENGTH_LONG).show();
+    }
+
+    private void resultCanceled() {
+        Toast.makeText(this, R.string.text_canceled, Toast.LENGTH_LONG).show();
     }
 }
